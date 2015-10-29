109d4c67c77c4d33e4720c298d2d5025534a90ce
Antti Laine <antti.laine@neotide.fi>
2015-10-29 09:42:04 +0200

    Modify to work with the new SharePoint site.

--- edustajat.py
+++ edustajat.py
@@ -4,33 +4,32 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'http://www.eduskunta.fi'
-LIST_PATH = '/triphome/bin/hex3000.sh'
+LIST_URL = 'https://www.eduskunta.fi/FI/kansanedustajat/Sivut/Kansanedustajat-aakkosjarjestyksessa.aspx'
 
 
 def main():
     try:
-        r = requests.get(''.join((BASE_URL, LIST_PATH)))
+        r = requests.get(LIST_URL)
         r.raise_for_status()
     except requests.HTTPError:
         print 'Could not GET listing page'
         return 1
 
-    s = BeautifulSoup(r.content)
-    page_urls = s.find('div', 'listing').find_all('a')
+    s = BeautifulSoup(r.content, 'lxml')
+    page_urls = s.find_all('div', 'link-item')
     for page in page_urls:
-        path = page.get('href')
+        path = page.find('a').get('href')
         try:
-            r = requests.get(''.join((BASE_URL, path)))
+            r = requests.get(path)
             r.raise_for_status()
         except requests.HTTPError:
             print 'Could not GET page', path
             continue
-        s = BeautifulSoup(r.content)
+        s = BeautifulSoup(r.content, 'lxml')
         try:
-            email = s.find('span', 'emailAddress').text.replace('[at]', '@')
+            email = s.find(lambda tag: tag.get('href', '').startswith('mailto')).text
         except AttributeError:
-            sys.stderr.write('No email found: %s\n' % s)
+            sys.stderr.write('No email found in %s\n' % path)
         else:
             print email
 
